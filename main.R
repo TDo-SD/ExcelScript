711540a854d4b74688e4cb1c75be9894ae7cd78d
TDo-SD <99211646+TDo-SD@users.noreply.github.com>
2023-01-02 16:34:26 -0500

    Added input from jira and output csv ability

--- main.R
+++ main.R
@@ -1,3 +1,8 @@
-#Taking out the Title, Purpose, Assignees, ID Number, and Creation Date
-#Put into new sheet
+library("data.table")
+
+jiraData <- read.csv("JIRA URL")
+
+tableData <- fread(jiraData, select = c("Title", "Purpose", "Assignees", "ID Number", "Date Created"))
+
+write.csv(tableData, "LOCATION", row.names=FALSE)
 
