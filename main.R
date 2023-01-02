79f79a52e04010dc3b12fef29f41f709cf35ba0f
TDo-SD <99211646+TDo-SD@users.noreply.github.com>
2023-01-02 16:39:40 -0500

    Added comments to code

--- main.R
+++ main.R
@@ -1,8 +1,13 @@
+#This is the package used to make the code
 library("data.table")
 
+#This is the intake data frame received from the JIRA dashboard CSV
 jiraData <- read.csv("JIRA URL")
 
+#This is the data frame created after a quick filter of what's needed. The dashboard is constantly updating so this is all
+#that is need for our intents and purposes.
 tableData <- fread(jiraData, select = c("Title", "Purpose", "Assignees", "ID Number", "Date Created"))
 
+#This writes the csv into the file I need and exports it as a csv.
 write.csv(tableData, "LOCATION", row.names=FALSE)
 
